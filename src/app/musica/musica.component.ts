8178a02eea9d7647de24dbf22464bb190858eb97
ErikS <erik.sambell@incentro.com>
2021-05-06 20:07:40 +0200

    first kick

--- src/app/musica/musica.component.ts
+++ src/app/musica/musica.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { from, of } from 'rxjs';
-import { delay, mergeMap, concatMap } from 'rxjs/internal/operators';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { from, interval, Observable, of } from 'rxjs';
+import { delay, mergeMap, concatMap, share, concatAll, tap } from 'rxjs/internal/operators';
 
 @Component({
   selector: 'app-musica',
@@ -10,23 +10,34 @@ import { delay, mergeMap, concatMap } from 'rxjs/internal/operators';
 export class MusicaComponent implements OnInit {
 
   bassline: string[] = [];
+  beat$: any;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.getBassline();
+    /**
+     * convert ms per beat
+     * one tricky thing is dealing with unicast vs multicast observables.
+     * An interval actually starts a new time source for every subscription, meaning it's unicast.
+     */
+
+    // this.beat$ = interval(msPerBeat).pipe(
+    //   share(), // turn it multicast, so all subscribers to beat get called up the same time
+    // )
+
+    this.beat$ = this.getBassline(this.bpmToMs(100));
   }
 
-  getBassline() {
+  private bpmToMs = (bpm: number) => (1000 * 60) / bpm;
+
+  public getBassline(msPerBeat: number): Observable<string> {
     for (let i = 0; i < 100; i++) this.bassline.push('bass');
 
-    const bpm = 130;
-    const interval = 60 / bpm * 1000;
-
-    of(this.bassline).pipe(
-      mergeMap((bass: any) => from(bass)),
-      concatMap(x => of(x).pipe(delay(interval)))
-    ).subscribe(y => console.log(y));
+    return of(this.bassline).pipe(
+      concatAll(),
+      concatMap((bass: string) => of(bass).pipe(delay(msPerBeat))),
+      tap(hee => console.log(hee)),
+    );
   }
 
 
