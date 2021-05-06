0bcde4fbbce08c94f95e548aa48f5aaaa3505f68
ErikS <erik.sambell@incentro.com>
2021-05-06 09:38:36 +0200

    we have a bassline (in the logs)

--- src/app/musica/musica.component.ts
+++ src/app/musica/musica.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { from, of } from 'rxjs';
+import { delay, mergeMap, concatMap } from 'rxjs/internal/operators';
 
 @Component({
   selector: 'app-musica',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MusicaComponent implements OnInit {
 
+  bassline: string[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.getBassline();
   }
 
+  getBassline() {
+    for (let i = 0; i < 100; i++) this.bassline.push('bass');
+
+    const bpm = 130;
+    const interval = 60 / bpm * 1000;
+
+    of(this.bassline).pipe(
+      mergeMap((bass: any) => from(bass)),
+      concatMap(x => of(x).pipe(delay(interval)))
+    ).subscribe(y => console.log(y));
+  }
+
+
+
 }
+
