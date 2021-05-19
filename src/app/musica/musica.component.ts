14bf775de6be865cf45d7048a195363e56a6efdc
ErikS <erik.sambell@incentro.com>
2021-05-19 20:36:47 +0200

    added offbeat

--- src/app/musica/musica.component.ts
+++ src/app/musica/musica.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { interval, Observable, of } from 'rxjs';
-import { delay, concatMap, tap, scan, share, map } from 'rxjs/internal/operators';
+import { delay, concatMap, tap, scan, share, map, switchMap } from 'rxjs/internal/operators';
 
 @Component({
   selector: 'app-musica',
@@ -10,8 +10,8 @@ import { delay, concatMap, tap, scan, share, map } from 'rxjs/internal/operators
 export class MusicaComponent implements OnInit {
 
   beat$: Observable<number[]> | undefined;
-
-  constructor() { }
+  offbeat$: Observable<number[]> | undefined;
+  bpmToMs = 0;
 
   ngOnInit(): void {
     /**
@@ -19,9 +19,22 @@ export class MusicaComponent implements OnInit {
      * one tricky thing is dealing with unicast vs multicast observables.
      * An interval actually starts a new time source for every subscription, meaning it's unicast.
      */
+    this.bpmToMs = this.getBpmToMs(140);
   }
 
-  private bpmToMs = (bpm: number) => (1000 * 60) / bpm;
+  public startMusica(): void {
+    this.beat$ = this.getBassline(this.bpmToMs);
+    this.offbeat$ = this.beat$.pipe(
+      delay(this.bpmToMs / 2)
+    )
+  }
+
+  public stopMusica(): void {
+    this.beat$ = undefined;
+    this.offbeat$ = undefined;
+  }
+
+  private getBpmToMs = (bpm: number) => (1000 * 60) / bpm;
 
   private getBassline(msPerBeat: number): Observable<number[]> {
     return interval(msPerBeat).pipe(
@@ -29,14 +42,6 @@ export class MusicaComponent implements OnInit {
       concatMap((kick: number) => of(kick).pipe(delay(msPerBeat))),
       map((kick: number) => [kick])
     );    
-  }
-
-  public startMusica(bpm: 140): void {
-    this.beat$ = this.getBassline(this.bpmToMs(bpm));
-  }
-
-  public stopMusica(): void {
-    this.beat$ = undefined;
   }
 
 }
