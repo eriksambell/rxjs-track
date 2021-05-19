dc1717293af3f730b3426aa76cd9026e93453cd9
ErikS <erik.sambell@incentro.com>
2021-05-19 20:22:29 +0200

    added play/stop button

--- src/app/musica/musica.component.ts
+++ src/app/musica/musica.component.ts
@@ -19,19 +19,24 @@ export class MusicaComponent implements OnInit {
      * one tricky thing is dealing with unicast vs multicast observables.
      * An interval actually starts a new time source for every subscription, meaning it's unicast.
      */
-
-    this.beat$ = this.getBassline(this.bpmToMs(140)).pipe(tap(console.log));
   }
 
   private bpmToMs = (bpm: number) => (1000 * 60) / bpm;
 
-  public getBassline(msPerBeat: number): Observable<number[]> {
+  private getBassline(msPerBeat: number): Observable<number[]> {
     return interval(msPerBeat).pipe(
       share(), // turn it multicast, so all subscribers to beat get called up the same time
       concatMap((kick: number) => of(kick).pipe(delay(msPerBeat))),
       map((kick: number) => [kick])
     );    
+  }
 
+  public startMusica(bpm: 140): void {
+    this.beat$ = this.getBassline(this.bpmToMs(bpm));
+  }
+
+  public stopMusica(): void {
+    this.beat$ = undefined;
   }
 
 }
