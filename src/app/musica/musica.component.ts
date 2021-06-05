892300f678665e607192125ae702da798f0aa9f9
ErikS <erik.sambell@incentro.com>
2021-06-05 15:05:25 +0200

    tweaks

--- src/app/musica/musica.component.ts
+++ src/app/musica/musica.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { interval, Observable, of, Subject } from 'rxjs';
 import { delay, concatMap, share, map, filter, takeUntil } from 'rxjs/internal/operators';
 
-type MusicLabel = 'beat' | 'claps' | 'melody' | 'hiHat';
+type Sample = 'beat' | 'claps' | 'melody' | 'hiHat';
 
 @Component({
   selector: 'app-musica',
@@ -19,34 +19,21 @@ export class MusicaComponent implements OnInit {
   bpmToMs = 0;
 
   music = {
-    beat: {
-      volume: 80,
-      sample: 'kick.wav'
-    },
-    claps: {
-      volume: 25,
-      sample: 'hh-closed.wav'
-    },
-    hiHat: {
-      volume: 25,
-      sample: 'hh-open.wav'
-    },
-    melody: {
-      volume: 25,
-      sample: 'big-room-bass-line_125bpm_D.wav'
-    },
+    beat: {volume: 80, sample: 'kick.wav'},
+    claps: {volume: 25, sample: 'hh-closed.wav'},
+    hiHat: {volume: 25, sample: 'hh-open.wav'},
+    melody: {volume: 40, sample: 'big-room-bass-line_125bpm_D.wav'},
   }
 
   destroy$ = new Subject<void>();
 
   ngOnInit(): void {
     /**
-     * convert ms per beat
      * one tricky thing is dealing with unicast vs multicast observables.
      * An interval actually starts a new time source for every subscription, meaning it's unicast.
      */
     this.bpmToMs = this.getBpmToMs(125);
-    this.startMusica();
+    setTimeout(() => this.startMusica(), 500);
   }
 
   public startMusica(): void {
@@ -58,17 +45,15 @@ export class MusicaComponent implements OnInit {
 
     this.claps$ = this.beat$.pipe(
       filter((val, index) => index % 2 === 0),
-      map((halfbeat: number, index: number) => index)
-    )
-
-    this.melody$ = this.beat$.pipe(
-      filter((val, index) => index % 16 === 0),
     )
 
     this.hiHats$ = this.beat$.pipe(
       delay(this.bpmToMs / 2)
     )
 
+    this.melody$ = this.beat$.pipe(
+      filter((val, index) => index % 16 === 0),
+    )
 
     this.beat$.subscribe(() => this.createAudio('beat'));
     this.claps$.subscribe(() => this.createAudio('claps'));
@@ -80,23 +65,29 @@ export class MusicaComponent implements OnInit {
     this.destroy$.next();
   }
 
+  public setVolume(volume: string, label: Sample) {
+    this.music[label].volume = Number(volume);
+  }
+
   private getBpmToMs = (bpm: number) => (1000 * 60) / bpm;
 
+  /**
+   * concatMap does not subscribe to the next observable until the previous completes, 
+   * the value from the source delayed by xx ms will be emitted first
+   * @param msPerBeat 
+   * @returns  an observable every x ms
+   */
   private getBassline(msPerBeat: number): Observable<number> {
     return interval(msPerBeat).pipe(
-      concatMap((kick: number) => of(kick).pipe(delay(msPerBeat)))
+      concatMap((beat: number) => of(beat))
     );
   }
 
-  private createAudio(label: MusicLabel) {
+  private createAudio(label: Sample) {
     let audio = new Audio();
     audio.src = `../../assets/samples/${this.music[label].sample}`
     audio.volume = this.music[label].volume / 100;
     audio.play()
-  }
-
-  public setVolume(volume: string, label: MusicLabel) {
-    this.music[label].volume = Number(volume);
   }
 
 }
