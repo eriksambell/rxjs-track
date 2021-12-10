3c36ea2758ab32129890c63ffb021b64faaff1ac
ErikS <erik.sambell@incentro.com>
2021-12-10 13:02:07 +0100

    created prettier file, implemented for musica comp

--- src/app/musica/musica.component.ts
+++ src/app/musica/musica.component.ts
@@ -7,16 +7,15 @@ type Sample = 'beat' | 'claps' | 'melody' | 'hiHat';
 @Component({
   selector: 'app-musica',
   templateUrl: './musica.component.html',
-  styleUrls: ['./musica.component.scss']
+  styleUrls: ['./musica.component.scss'],
 })
 export class MusicaComponent {
-
   music = {
     beat: { volume: 80, sample: 'kick.wav' },
     claps: { volume: 25, sample: 'hh-closed.wav' },
     hiHat: { volume: 25, sample: 'hh-open.wav' },
     melody: { volume: 40, sample: 'big-room-bass-line_125bpm_D.wav' },
-  }
+  };
 
   destroy$ = new Subject<void>();
 
@@ -29,14 +28,11 @@ export class MusicaComponent {
     const msPerBeat = this.bpmToMs(125);
 
     // create observables
-    const beat$: Observable<number> = interval(msPerBeat).pipe(
-      takeUntil(this.destroy$),
-      share()
-    );
+    const beat$: Observable<number> = interval(msPerBeat).pipe(takeUntil(this.destroy$), share());
 
-    const claps$: Observable<number> = beat$.pipe(filter((value, index) => index % 2 === 0));
+    const claps$: Observable<number> = beat$.pipe(filter((_, index) => index % 2 === 0));
     const hiHats$: Observable<number> = beat$.pipe(delay(msPerBeat / 2));
-    const melody$: Observable<number> = beat$.pipe(filter((value, index) => index % 16 === 0));
+    const melody$: Observable<number> = beat$.pipe(filter((_, index) => index % 16 === 0));
 
     // subscribe to observables and create audio
     beat$.subscribe(() => this.createAudio('beat'));
@@ -57,10 +53,8 @@ export class MusicaComponent {
 
   private createAudio(label: Sample) {
     let audio = new Audio();
-    audio.src = `assets/samples/${this.music[label].sample}`
+    audio.src = `assets/samples/${this.music[label].sample}`;
     audio.volume = this.music[label].volume / 100;
-    audio.play()
+    audio.play();
   }
-
 }
-
